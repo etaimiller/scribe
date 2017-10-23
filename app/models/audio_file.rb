bf4bb805a4180fc2ce79cf0789cea9ff5f98aec9
Emile Silvis <emile@silvis.co.za>
2017-10-23 22:36:57 +0200

    Revert "Merge branch 'feat/filesize' into feat/stripe"
    This reverts commit 706ed3d52c5f110081f4bcd63fcb01236463c83c, reversing
    changes made to 004481386cc9ce880836dfa68c88f24ef7525eb3.

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -1,15 +1,7 @@
 class AudioFile < ApplicationRecord
   mount_uploader :audio, AudioUploader
 
-  before_save :update_audio_file_size
-
   validates :email,
             :audio,
             presence: true
-
-  def update_audio_file_size
-    if audio.present? && audio_changed?
-      self.file_size = audio.file.size
-    end
-  end
 end
--- db/migrate/20171023202301_add_file_size_to_audio_file.rb
+++
@@ -1,5 +0,0 @@
-class AddFileSizeToAudioFile < ActiveRecord::Migration[5.1]
-  def change
-    add_column :audio_files, :file_size, :float
-  end
-end
